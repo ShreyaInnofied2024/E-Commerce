8da3a67592006403fab764c9a4885568228705ad
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-17 18:14:23 +0530

    Delete E-Commerce directory

--- E-Commerce/delete.php
+++
@@ -1,4 +0,0 @@
-<?php
-include('product.php');
-$product = new Product('Apple',30,56);
-$product->delete($_GET['id']);
--- E-Commerce/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('product.php');
-$product = new Product($_POST['name'], $_POST['quantity'], $_POST['price']);
-$product->update();
-
- 
